6587f55b1cc3b6c8fd0a4ae232270009c1c2c5eb
Katherine <Katherine.Saung@peopleshores.com>
2024-11-05 16:58:28 -0500

    updated User entity with lombak annotations

--- src/main/java/org/example/user/User.java
+++ src/main/java/org/example/user/User.java
@@ -1,5 +1,23 @@
 package org.example.user;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.security.Principal;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
+
 
 }
