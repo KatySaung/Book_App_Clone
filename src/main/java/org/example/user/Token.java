f181953e0604a65ff36a26fcc1a47d9d07e60bc9
Katherine <Katherine.Saung@peopleshores.com>
2024-11-08 16:19:23 -0500

    created Token class

---
+++ src/main/java/org/example/user/Token.java
@@ -0,0 +1,17 @@
+package org.example.user;
+
+import jakarta.persistence.Entity;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+
+}
